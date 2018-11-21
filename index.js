59c005adbf0ddc37adc72cf1c0f4a1c1c2d119a3
Ryan Romano <ryanaromano@gmail.com>
2018-11-21 13:59:16 -0500

    fixes #5 - separate listen logic to bin

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
-const host = 'localhost';
 
 app.get('/', (req, res) => {
     res.send('Hello world! :)');
 });
 
-app.listen(port, host, () => {
-    console.log(`Listening on ${host}:${port}!`);
-});
+module.exports = app;
