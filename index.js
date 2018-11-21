351654b66a4186d0a3f968efbdcb1c18b2f4cbfc
Ryan Romano <ryanaromano@gmail.com>
2018-11-21 13:34:18 -0500

    Fixes #3 - set up express to listen on localhost3000

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello world :)');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const host = 'localhost';
+
+app.get('/', (req, res) => {
+    res.send('Hello world! :)');
+});
+
+app.listen(port, host, () => {
+    console.log(`Listening on ${host}:${port}!`);
+});
