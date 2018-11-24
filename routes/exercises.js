195217a8dcab70c3ba81b8b4e5712d2dd030031f
Ryan Romano <ryanaromano@gmail.com>
2018-11-24 16:37:28 -0500

    Fixes #29

--- routes/exercises.js
+++ routes/exercises.js
@@ -16,4 +16,19 @@ exercisesRouter.get('/', (req, res) => {
     );
 });
 
+exercisesRouter.get('/:id', (req, res) => {
+    pool.query(
+        'SELECT exercises.id, exercises.exercise_name, muscle_groups.muscle_group, exercises.muscle_group_id FROM exercises ' + 
+        'INNER JOIN muscle_groups ON exercises.muscle_group_id = muscle_groups.id WHERE exercises.id = $1',
+        [req.params.id],
+        (error, response) => {
+            if(error){
+                console.log(error);
+            } else {
+                res.send(response.rows);
+            }
+        }
+    );
+});
+
 module.exports = exercisesRouter;
