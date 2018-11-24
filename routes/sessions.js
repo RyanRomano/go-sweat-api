7e42b9128183514ada66fa094a04be662b5f84e7
Ryan Romano <ryanaromano@gmail.com>
2018-11-24 13:41:45 -0500

    Fixes #12 - get session by id

--- routes/sessions.js
+++ routes/sessions.js
@@ -15,6 +15,19 @@ sessionsRouter.get('/', (req, res) => {
     );
 });
 
+sessionsRouter.get('/:id', (req, res) => {
+    pool.query(
+        'SELECT id, date, muscles_worked FROM sessions WHERE sessions.id = $1',[req.params.id],
+        (error, response) => {
+            if(error){
+                console.log(error);
+            } else {
+                res.send(response.rows);
+            }
+        }
+    );
+});
+
 // get, post, put, delete endpoints here
 
 module.exports = sessionsRouter;
