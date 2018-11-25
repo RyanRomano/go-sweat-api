2046e368dd826cadbabc45da2669e612018c9b77
Ryan Romano <ryanaromano@gmail.com>
2018-11-25 15:02:26 -0500

    Separated index route file from table routes

--- routes/routers/sessions.js
+++ routes/routers/sessions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const sessionsRouter = express.Router();
-const pool = require('../db');
+const pool = require('../../db');
 
 sessionsRouter.get('/', (req, res) => {
     pool.query(
