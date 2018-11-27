7810d8c2aab64029d180cc6fbc554a33f7814424
Ryan Romano <ryanaromano@gmail.com>
2018-11-27 12:24:55 -0500

    Add cors config

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes/index.js');
+const cors = require('cors');
+
+app.use(cors({
+    origin: 'http://localhost:1234'
+}));
 
 app.use(bodyParser.json());
 app.use('/', routes);
