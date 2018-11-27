44dfbc261d90c1dda79eef6c1577da1c70fc959e
Ryan Romano <ryanaromano@gmail.com>
2018-11-27 14:03:07 -0500

    Get all workouts of a session matching with workout.session_id

--- routes/routers/sessions.js
+++ routes/routers/sessions.js
@@ -42,6 +42,25 @@ sessionsRouter.post('/', (req, res) => {
     );
 });
 
+
+sessionsRouter.get('/:id/workouts', (req, res) => {
+    pool.query(
+        'SELECT w.id, w.session_id, s.date, ex.exercise_name, eq.equipment_type, w.sets, w.reps, w.set1, w.set2, w.set3, w.notes ' +
+        'FROM workouts w JOIN sessions s ON w.session_id = s.id ' +
+        'JOIN exercises ex ON w.exercise_id = ex.id ' +
+        'JOIN equipment eq ON w.equipment_id = eq.id ' + 
+        'WHERE w.session_id = $1',
+        [req.params.id],
+        (error, response) => {
+            if(error){
+                console.log(error);
+            } else {
+                res.send(response.rows);
+            }
+        }
+    );
+});
+
 sessionsRouter.delete('/:id', (req, res) => {
     pool.query(
         'DELETE FROM sessions WHERE sessions.id = $1', 
